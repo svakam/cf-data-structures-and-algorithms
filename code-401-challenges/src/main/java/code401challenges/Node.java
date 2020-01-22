56a96568e4fa0a8c1a231911bf668cb0a59db4c7
Vik Akam <vikramakam@hotmail.com>
2020-01-22 11:28:57 -0800

    initialize animalshelter

---
+++ code-401-challenges/src/main/java/code401challenges/Node.java
@@ -0,0 +1,13 @@
+package code401challenges;
+
+public class Node {
+    // variables
+    int value;
+    Node next;
+
+    // constructor
+    public Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
---
+++ code-401-challenges/src/main/java/code401challenges/Queue.java
@@ -0,0 +1,44 @@
+package code401challenges;
+
+import java.util.NoSuchElementException;
+
+public class Queue {
+    Node front;
+    Node rear;
+
+    // adds new node to back of queue
+    public void enqueue(int value) {
+        Node newNode = new Node(value);
+        if (front == null) {
+            front = newNode;
+        } else {
+            rear.next = newNode;
+        }
+        rear = newNode;
+    }
+
+    // removes node from front of the queue
+    public int dequeue() {
+        Node temp = front;
+        if (front == null) {
+            throw new NoSuchElementException("Cannot dequeue from an empty queue");
+        }
+        front = front.next;
+        temp.next = null;
+        return temp.value;
+    }
+
+    // returns node value in front of queue without removing it from queue
+    public int peek() {
+        if (front == null) {
+            throw new NoSuchElementException("Cannot peek with an empty queue");
+        }
+        return front.value;
+    }
+
+    // return boolean for empty vs non empty queue
+    public boolean isEmpty() {
+        return front == null;
+    }
+}
+
